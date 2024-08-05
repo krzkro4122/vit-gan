e2e9ef5816e73b46228cde51dd820231ca740689
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-08-05 16:49:16 +0200

    moved stuff to utils

--- src/v2/training.py
+++ src/v2/training.py
@@ -1,62 +1,29 @@
 import datetime
 import os
-from random import randint
-from typing import Union
 import torch
-import rich
-import numpy as np
 import torch.nn.functional as F
-from torch.optim.adam import Adam
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data import DataLoader
 import torchvision.utils as vutils
+
+from random import randint
+from typing import Union
+from torch.optim.adam import Adam
+from torch.utils.data import DataLoader
 from torchmetrics.image.fid import FrechetInceptionDistance
-from pytorch_fid.inception import InceptionV3
+
 import src.v2.modules as modules
-
-START_TIME = datetime.datetime.now()
-BASE_DIR = os.getenv("SCRATCH", "~")
-OUTPUT_DIR = f"{BASE_DIR}/output"
-SAVE_DIR = os.path.join(OUTPUT_DIR, START_TIME.strftime("%Y%m%d-%H%M%S"))
-IMAGES_DIR = os.path.join(SAVE_DIR, "images")
-NOISE_DIR = os.path.join(SAVE_DIR, "noise")
-
-
-def log(message: str):
-    rich.print(message)
-    with open(os.path.join(SAVE_DIR, "training.log"), "a", encoding="utf-8") as handle:
-        handle.write(message + "\n")
-
-
-class ToTensorUInt8(object):
-    def __call__(self, pic):
-        # Make the array writable by copying it
-        img = np.array(pic, np.uint8, copy=True)
-        img = torch.from_numpy(img)
-        img = img.permute(2, 0, 1).contiguous()  # Convert to CxHxW
-        return img
-
-
-def convert_to_uint8(images):
-    images = (images * 127.5 + 127.5).clamp(0, 255).to(torch.uint8)
-    return images
-
-
-def calculate_inception_score(images, batch_size=32, splits=10):
-    # Convert images to float32 and normalize to [0, 1]
-    images = images.float() / 255.0
-    model = InceptionV3([InceptionV3.BLOCK_INDEX_BY_DIM[2048]]).eval().to(images.device)
-    with torch.no_grad():
-        preds = model(images)[0]
-    preds = F.softmax(preds, dim=1)
-    scores = []
-    for i in range(splits):
-        part = preds[i * len(preds) // splits : (i + 1) * len(preds) // splits, :]
-        kl_div = part * (torch.log(part) - torch.log(part.mean(dim=0, keepdim=True)))
-        kl_div = kl_div.sum(dim=1)
-        scores.append(kl_div.mean().exp().item())
-    return torch.tensor(scores).mean(), torch.tensor(scores).std()
+from src.v2.utils import (
+    log,
+    ToTensorUInt8,
+    convert_to_uint8,
+    calculate_inception_score,
+    OUTPUT_DIR,
+    SAVE_DIR,
+    START_TIME,
+    IMAGES_DIR,
+    NOISE_DIR,
+)
 
 
 def run():
---
+++ src/v2/utils.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+import torch
+import rich
+import numpy as np
+import torch.nn.functional as F
+from pytorch_fid.inception import InceptionV3
+
+
+START_TIME = datetime.datetime.now()
+BASE_DIR = os.getenv("SCRATCH", "~")
+OUTPUT_DIR = f"{BASE_DIR}/output"
+SAVE_DIR = os.path.join(OUTPUT_DIR, START_TIME.strftime("%Y%m%d-%H%M%S"))
+IMAGES_DIR = os.path.join(SAVE_DIR, "images")
+NOISE_DIR = os.path.join(SAVE_DIR, "noise")
+
+
+def log(message: str):
+    rich.print(message)
+    with open(os.path.join(SAVE_DIR, "training.log"), "a", encoding="utf-8") as handle:
+        handle.write(message + "\n")
+
+
+class ToTensorUInt8(object):
+    def __call__(self, pic):
+        # Make the array writable by copying it
+        img = np.array(pic, np.uint8, copy=True)
+        img = torch.from_numpy(img)
+        img = img.permute(2, 0, 1).contiguous()  # Convert to CxHxW
+        return img
+
+
+def convert_to_uint8(images):
+    images = (images * 127.5 + 127.5).clamp(0, 255).to(torch.uint8)
+    return images
+
+
+def calculate_inception_score(images, batch_size=32, splits=10):
+    # Convert images to float32 and normalize to [0, 1]
+    images = images.float() / 255.0
+    model = InceptionV3([InceptionV3.BLOCK_INDEX_BY_DIM[2048]]).eval().to(images.device)
+    with torch.no_grad():
+        preds = model(images)[0]
+    preds = F.softmax(preds, dim=1)
+    scores = []
+    for i in range(splits):
+        part = preds[i * len(preds) // splits : (i + 1) * len(preds) // splits, :]
+        kl_div = part * (torch.log(part) - torch.log(part.mean(dim=0, keepdim=True)))
+        kl_div = kl_div.sum(dim=1)
+        scores.append(kl_div.mean().exp().item())
+    return torch.tensor(scores).mean(), torch.tensor(scores).std()
