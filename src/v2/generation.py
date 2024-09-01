c66d36cd1e0b0553b46b06cf79e228ae10a83c6c
krzkro4122 <krolkrzysztof1999@gmail.comm>
2024-09-01 15:42:43 +0200

    corrected generation test

--- src/v2/generation.py
+++ src/v2/generation.py
@@ -4,32 +4,23 @@ import torch
 import torchvision.utils as vutils
 
 from src.v2.modules import ViTGAN
+from src.v2.utils import Config
 
 
 def test():
-    # Hyperparameters
-    img_size = 32
-    patch_size = 4
-    in_chans = 3
-    embed_dim = 256
-    depth = 6
-    num_heads = 8
-    mlp_ratio = 4.0
-    dropout_rate = 0.2
-    batch_size = 64
-    noise_shape = in_chans, img_size, img_size
+    c = Config()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Initialize ViTGAN
     vit_gan = ViTGAN(
-        img_size,
-        patch_size,
-        in_chans,
-        embed_dim,
-        depth,
-        num_heads,
-        mlp_ratio,
-        dropout_rate,
+        c.image_size,
+        c.patch_size,
+        c.input_chanels,
+        c.embeddings_dimension,
+        c.transformer_blocks_count,
+        c.attention_heads_count,
+        c.mlp_ratio,
+        c.dropout_rate,
     ).to(device)
 
     output_dir = "20240805-223515"
@@ -47,17 +38,23 @@ def test():
     vit_gan.eval()
 
     def construct_noise():
-        return torch.randn(batch_size, *noise_shape, device=device)
+        return torch.randn(
+            c.batch_size,
+            c.input_chanels,
+            c.image_size,
+            c.image_size,
+            device=device,
+        )
 
     def save_images(model: ViTGAN):
         noise = construct_noise()
         sample_images = [
-            model.generator(noise).detach().cpu()[i] for i in range(batch_size)
+            model.generator(noise).detach().cpu()[i] for i in range(c.batch_size)
         ]
         images_save_path = os.path.join(test_dir, "generated_images.png")
         noise_save_path = os.path.join(test_dir, "noise.png")
         vutils.save_image(sample_images, images_save_path, nrow=8, normalize=True)
         vutils.save_image(noise, noise_save_path, nrow=8, normalize=True)
-        print(f"{batch_size} Generated images and noise saved to {test_dir}")
+        print(f"{c.batch_size} Generated images and noise saved to {test_dir}")
 
     save_images(vit_gan)
